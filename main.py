5dec824c8a94b9c18a8c14bdfef1fd2990a019b9
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-01 12:14:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ from vars import API_ID, API_HASH, BOT_TOKEN
 centered_text = "◦•●◉✿ 𝕰𝖓𝖌𝖎𝖓𝖊𝖊𝖗𝖘 𝕭𝖆𝖇𝖚 ✿◉●•◦".center(40)
 
 # Environment variables for API credentials
-API_ID = os.environ.get("API_ID", "21705536")
-API_HASH = os.environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
+API_ID = os.environ.get("API_ID", "27775431")
+API_HASH = os.environ.get("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Initialize the bot
